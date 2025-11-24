832872d69351fe8e08d1cffc1a71dc709ccfa352
masako421 <tanakamasatyo@gmail.com>
2025-11-24 09:45:00 +0900

    Create megalodon.lua

---
+++ megalodon.lua
@@ -0,0 +1,21 @@
+local event = game.ReplicatedStorage:WaitForChild("debug_force_megalodon")
+local model = game.ServerStorage:WaitForChild("Phantom Megalodon")
+
+event.OnServerEvent:Connect(function(player)
+    local shark = model:Clone()
+    shark.Parent = workspace
+end)
+--openscriptfuck--openscriptfuck--openscriptfuck
+--openscriptfuck
+--openscriptfuck
+--openscriptfuck
+--openscriptfuck
+--openscriptfuck
+
+--openscriptfuck
+--openscriptfuck
+--openscriptfuck
+--openscriptfuck
+--openscriptfuck
+--openscriptfuck
+--openscriptfuck
