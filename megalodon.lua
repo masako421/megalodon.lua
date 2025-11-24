b70dbfb1a729ca2522648384b85c3ec4d58f51e5
masako421 <tanakamasatyo@gmail.com>
2025-11-24 09:53:45 +0900

    Update megalodon.lua

--- megalodon.lua
+++ megalodon.lua
@@ -128,13 +128,20 @@
 --openscriptfuck
 --openscriptfuck
 
-local event = game.ReplicatedStorage.event:WaitForChild("debug_force_megalodon")
-local model = game.ServerStorage:WaitForChild("Phantom Megalodon")
+local event = game.ReplicatedStorage.events:WaitForChild("debug_force_megalodon")
+
+local model = game.ReplicatedStorage.client.legacyControllers
+    :WaitForChild("CutsceneController")
+    :WaitForChild("Cutscenes")
+    :WaitForChild("MegalodonCapture")
+    :WaitForChild("Models Olds")
+    :WaitForChild("Phantom Megalodon")
 
 event.Event:Connect(function()
     local shark = model:Clone()
     shark.Parent = workspace
 end)
+
 --openscriptfuck--openscriptfuck--openscriptfuck
 --openscriptfuck
 --openscriptfuck
